006cbb3ef0e2bff5325a090edfcd1fff082b4852
The Cocoa <thecocoa@pm.me>
2026-04-13 03:36:09 +0000

    commit

--- chapter08/project1/js/ch08-proj01.js
+++ chapter08/project1/js/ch08-proj01.js
@@ -1,6 +1,38 @@
 
-const tax_rate = prompt('Enter tax rate (0.10)');
-const shipping_threshold = prompt('Enter shipping threshold (1000)');
+const tax_rate = parseFloat(prompt('Enter tax rate (0.10)'));
+const shipping_threshold = parseFloat(prompt('Enter shipping threshold (1000)'));
 
-/* add loop and other code here ... in this simple exercise we are not
-   going to concern ourselves with minimizing globals, etc */
+let subtotal = 0;
+
+for (let i = 0; i < cart.length; i++) {
+    let item = cart[i];
+
+    let total = calculateTotal(item.quantity, item.product.price);
+    subtotal += total;
+
+    outputCartRow(item, total);
+}
+
+let tax = subtotal * tax_rate;
+let shipping = subtotal > shipping_threshold ? 0 : 40;
+let grandTotal = subtotal + tax + shipping;
+
+document.write("<tr class='totals'>");
+document.write("<td colspan='4'>Subtotal</td>");
+document.write("<td>$" + subtotal.toFixed(2) + "</td>");
+document.write("</tr>");
+
+document.write("<tr class='totals'>");
+document.write("<td colspan='4'>Tax</td>");
+document.write("<td>$" + tax.toFixed(2) + "</td>");
+document.write("</tr>");
+
+document.write("<tr class='totals'>");
+document.write("<td colspan='4'>Shipping</td>");
+document.write("<td>$" + shipping.toFixed(2) + "</td>");
+document.write("</tr>");
+
+document.write("<tr class='totals'>");
+document.write("<td colspan='4' class='focus'>Grand Total</td>");
+document.write("<td class='focus'>$" + grandTotal.toFixed(2) + "</td>");
+document.write("</tr>");
--- chapter08/project1/js/functions.js
+++ chapter08/project1/js/functions.js
@@ -1,5 +1,34 @@
 /* define your functions here */
 
+function calculateTotal(quantity, price) {
+//passed a quantity and price and return their product
+    return quantity * price;
+}
+
+function outputCartRow(item, total) {
+// must use documen.write() calls to display a row of the table using the passed data.  
+// Use the toFixed() metyof of the number variables to display two decimal places.
+
+
+    document.write("<tr>");
+
+    document.write("<td><img src='images/" 
+        + item.product.filename + "'></td>");
+
+    document.write("<td>" 
+        + item.product.title + "</td>");
+
+    document.write("<td>" 
+        + item.quantity + "</td>");
+
+    document.write("<td>$" 
+        + item.product.price.toFixed(2) + "</td>");
+
+    document.write("<td>$" 
+        + total.toFixed(2) + "</td>");
+
+    document.write("</tr>");  
+}
 
 
 
