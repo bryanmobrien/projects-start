9cd9a3491e252657d1b3bee9eaeb57a4317e4bf3
bobrien <bobrien@tank2-linux.home.arpa>
2026-04-12 22:30:35 -0500

    first commit

--- chapter14/project1/ch14-proj1.php
+++ chapter14/project1/ch14-proj1.php
@@ -1,9 +1,67 @@
 <?php
 include 'includes/travel-config.inc.php';
 
+/*
+ * Find the PDO connection created by travel-config.inc.php.
+ * This makes the page a little more tolerant of whatever variable name
+ * your instructor used in the config file.
+ */
+function getPDOConnection(): PDO {
+    foreach ($GLOBALS as $value) {
+        if ($value instanceof PDO) {
+            $value->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $value;
+        }
+    }
+    throw new Exception('No PDO connection found. Check includes/travel-config.inc.php');
+}
 
+$pdo = getPDOConnection();
+
+$continent = $_GET['continent'] ?? '0';
+$country   = $_GET['country'] ?? '0';
+$title     = trim($_GET['title'] ?? '');
+
+/* Dropdown data */
+$continentSql = "SELECT ContinentCode, ContinentName
+                 FROM continents
+                 ORDER BY ContinentName";
+$continentStmt = $pdo->query($continentSql);
+$continents = $continentStmt->fetchAll(PDO::FETCH_ASSOC);
+
+$countrySql = "SELECT ISO, CountryName
+               FROM countries
+               ORDER BY CountryName";
+$countryStmt = $pdo->query($countrySql);
+$countries = $countryStmt->fetchAll(PDO::FETCH_ASSOC);
+
+/* Main filtered gallery query */
+$sql = "SELECT ImageID, Title, Path
+        FROM imagedetails
+        WHERE 1=1";
+$params = [];
+
+if ($continent !== '0') {
+    $sql .= " AND ContinentCode = :continent";
+    $params[':continent'] = $continent;
+}
+
+if ($country !== '0') {
+    $sql .= " AND CountryCodeISO = :country";
+    $params[':country'] = $country;
+}
+
+if ($title !== '') {
+    $sql .= " AND Title LIKE :title";
+    $params[':title'] = '%' . $title . '%';
+}
+
+$sql .= " ORDER BY Title";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute($params);
+$images = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,42 +69,59 @@ include 'includes/travel-config.inc.php';
     <meta charset="utf-8">
     <title>Chapter 14</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-   
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="css/styles.css" />
-
 </head>
 
 <body>
     <header>
-        <form action="ch14-proj1.php" method="get" >
-          <div class="form-inline">
-          <select name="continent" >
-            <option value="0">Select Continent</option>
-          </select>     
-          
-          <select name="country">
-            <option value="0">Select Country</option>
-          </select>    
-          <input type="text"  placeholder="Search title" name=title>
-          <button type="submit" class="btn-primary">Filter</button>
-          <button type="submit" class="btn-secondary">Reset</button>
-          </div>
+        <form action="ch14-proj1.php" method="get">
+            <div class="form-inline">
+                <select name="continent">
+                    <option value="0">Select Continent</option>
+                    <?php foreach ($continents as $row) : ?>
+                        <option value="<?= htmlspecialchars($row['ContinentCode']) ?>"
+                            <?= ($continent === $row['ContinentCode']) ? 'selected' : '' ?>>
+                            <?= htmlspecialchars($row['ContinentName']) ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+
+                <select name="country">
+                    <option value="0">Select Country</option>
+                    <?php foreach ($countries as $row) : ?>
+                        <option value="<?= htmlspecialchars($row['ISO']) ?>"
+                            <?= ($country === $row['ISO']) ? 'selected' : '' ?>>
+                            <?= htmlspecialchars($row['CountryName']) ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+
+                <input type="text" placeholder="Search title" name="title"
+                       value="<?= htmlspecialchars($title) ?>">
+
+                <button type="submit" class="btn-primary">Filter</button>
+                <a href="ch14-proj1.php" class="btn-secondary">Reset</a>
+            </div>
         </form>
-    </header>   
-                                    
-    <main >
-        <ul >
+    </header>
 
-            <li>
-                link+img here
-            </li>        
-
-          </ul>       
-
-      
-      </main>
-
+    <main>
+        <ul>
+            <?php if (count($images) > 0) : ?>
+                <?php foreach ($images as $img) : ?>
+                    <li>
+                        <a href="detail.php?id=<?= urlencode($img['ImageID']) ?>">
+                            <img src="images/travel/square-medium/<?= htmlspecialchars($img['Path']) ?>"
+                                 alt="<?= htmlspecialchars($img['Title'] ?? 'Travel photo') ?>">
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <li>No matching images found.</li>
+            <?php endif; ?>
+        </ul>
+    </main>
 </body>
 
 </html>
--- chapter14/project1/detail.php
+++ chapter14/project1/detail.php
@@ -1,11 +1,72 @@
 <?php
-
 include 'includes/travel-config.inc.php';
 
+function getPDOConnection(): PDO {
+    foreach ($GLOBALS as $value) {
+        if ($value instanceof PDO) {
+            $value->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $value;
+        }
+    }
+    throw new Exception('No PDO connection found. Check includes/travel-config.inc.php');
+}
 
+$pdo = getPDOConnection();
 
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+$sql = "SELECT i.ImageID, i.Title, i.Description, i.Path, i.Exif, i.Colors,
+               i.ActualCreator, i.CreatorURL,
+               c.AsciiName AS CityName,
+               co.CountryName,
+               u.FirstName, u.LastName
+        FROM imagedetails i
+        LEFT JOIN cities c
+            ON i.CityCode = c.CityCode
+        LEFT JOIN countries co
+            ON i.CountryCodeISO = co.ISO
+        LEFT JOIN users u
+            ON i.UserID = u.UserID
+        WHERE i.ImageID = :id";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([':id' => $id]);
+$image = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$image) {
+    die('Image not found.');
+}
+
+$exif = [];
+if (!empty($image['Exif'])) {
+    $decoded = json_decode($image['Exif'], true);
+    if (is_array($decoded)) {
+        $exif = $decoded;
+    }
+}
+
+$colors = [];
+if (!empty($image['Colors'])) {
+    $decoded = json_decode($image['Colors'], true);
+    if (is_array($decoded)) {
+        $colors = $decoded;
+    }
+}
+
+$creatorName = trim(($image['FirstName'] ?? '') . ' ' . ($image['LastName'] ?? ''));
+if ($creatorName === '') {
+    $creatorName = $image['ActualCreator'] ?? 'Unknown';
+}
+
+$cityCountry = [];
+if (!empty($image['CityName'])) {
+    $cityCountry[] = $image['CityName'];
+}
+if (!empty($image['CountryName'])) {
+    $cityCountry[] = $image['CountryName'];
+}
+$locationText = count($cityCountry) ? implode(', ', $cityCountry) : 'Unknown location';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,36 +74,60 @@ include 'includes/travel-config.inc.php';
     <meta charset="utf-8">
     <title>Chapter 14</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-   
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="css/styles.css" />
-
 </head>
 
-<body>                                    
-   <main class="detail">
-      <div>
-         image here
-      </div>
-      <div>
-         <h1>title here</h1>
-         <h3>city, country</h3>
-         <p>description</p>
-         <div class="box">
-            <h3>Creator</h3>
+<body>
+    <main class="detail">
+        <div>
+            <img src="images/travel/large/<?= htmlspecialchars($image['Path']) ?>"
+                 alt="<?= htmlspecialchars($image['Title'] ?? 'Travel photo') ?>">
+        </div>
 
-         </div>
-         
-         <div class="box">
-            <h3>Camera</h3>
+        <div>
+            <h1><?= htmlspecialchars($image['Title'] ?? 'Untitled') ?></h1>
+            <h3><?= htmlspecialchars($locationText) ?></h3>
+            <p><?= htmlspecialchars($image['Description'] ?? '') ?></p>
 
-         </div>
-         <div class="box">
-            <h3>Colors</h3>
+            <div class="box">
+                <h3>Creator</h3>
+                <?php if (!empty($image['CreatorURL'])) : ?>
+                    <p>
+                        <a href="<?= htmlspecialchars($image['CreatorURL']) ?>">
+                            <?= htmlspecialchars($creatorName) ?>
+                        </a>
+                    </p>
+                <?php else : ?>
+                    <p><?= htmlspecialchars($creatorName) ?></p>
+                <?php endif; ?>
+            </div>
 
-         </div>
-      </div>
-   </main>
+            <div class="box">
+                <h3>Camera</h3>
+                <p>Make: <?= htmlspecialchars((string)($exif['make'] ?? 'N/A')) ?></p>
+                <p>Model: <?= htmlspecialchars((string)($exif['model'] ?? 'N/A')) ?></p>
+                <p>Exposure: <?= htmlspecialchars((string)($exif['exposure_time'] ?? 'N/A')) ?></p>
+                <p>Aperture: <?= htmlspecialchars((string)($exif['aperture'] ?? 'N/A')) ?></p>
+                <p>Focal Length: <?= htmlspecialchars((string)($exif['focal_length'] ?? 'N/A')) ?></p>
+                <p>ISO: <?= htmlspecialchars((string)($exif['iso'] ?? 'N/A')) ?></p>
+            </div>
+
+            <div class="box">
+                <h3>Colors</h3>
+                <?php if (count($colors) > 0) : ?>
+                    <?php foreach ($colors as $color) : ?>
+                        <div style="display:inline-block; margin:4px; text-align:center;">
+                            <div style="width:40px; height:40px; background:<?= htmlspecialchars($color) ?>; border:1px solid #000;"></div>
+                            <small><?= htmlspecialchars($color) ?></small>
+                        </div>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <p>No color data available.</p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
